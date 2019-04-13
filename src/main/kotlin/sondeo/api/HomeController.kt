9cc1dd356026d43845fa02431b4d8458bc1d84b2
Paulo Esteban <Paulo.Esteban@emc.com>
2019-04-13 18:19:12 +0100

    Added home controller

---
+++ src/main/kotlin/sondeo/api/HomeController.kt
@@ -0,0 +1,14 @@
+package sondeo.api
+
+import io.micronaut.http.MediaType
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller("/")
+class HomeController {
+
+    @Get(produces = [MediaType.TEXT_PLAIN])
+    fun index(): String {
+        return "Welcome to Sondeo!"
+    }
+}
